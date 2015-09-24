875927437ce9fd6d17308c79d151f7b775a6bc63
daiki_shimada_mba <sheema.sheemap@gmail.com>
2015-09-24 12:56:46 +0900

    create example data

--- example/example.cpp
+++ example/example.cpp
@@ -1,7 +1,9 @@
-#include <kadare/kadare.hpp>
+#include "../include/kadare/kadare.hpp"
 
 int main(int argc, char* argv[])
 {
-	kadare::DataManager x;
+	std::string data_dir = "data";
+	std::string data_file = "test_str_str.data";
+	kadare::DataManager<std::string, std::string> x(data_dir, data_file);
 	return 0;
 }
