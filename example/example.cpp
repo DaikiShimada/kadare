8c666de9c754d656fc8e3674b642d13ba8fffe87
daiki_shimada_mba <sheema.sheemap@gmail.com>
2015-09-25 12:27:50 +0900

    update data member and example, delete dbDir member

--- example/example.cpp
+++ example/example.cpp
@@ -1,10 +1,28 @@
 #include "../include/kadare/kadare.hpp"
+#include <iostream>
 #include <string>
 
 int main(int argc, char* argv[])
 {
-	std::string data_dir = "data";
-	std::string data_file = "test_str_str.data";
-	kadare::DataManager<std::string, std::string> x(data_dir, data_file);
+	std::string data_file = "data/test_str_str.data";
+	kadare::DataManager<std::string, std::string> x(data_file);
+
+	// load data
+	for (int i=0; i<x.getRecords(); ++i)
+	{
+		std::pair<std::vector<std::string>, std::vector<std::string> > d = x.load();
+		std::vector<std::string>::iterator key_itr = d.first.begin();
+		std::vector<std::string>::iterator value_itr = d.second.begin();
+		
+		for (key_itr=d.first.begin(); key_itr!=d.first.end(); ++key_itr)
+			std::cout << *key_itr << " ";
+
+		std::cout << "-> ";
+
+		for (value_itr=d.second.begin(); value_itr!=d.second.end(); ++value_itr)
+			std::cout << *value_itr << " ";
+
+		std::cout << std::endl;
+	}
 	return 0;
 }
--- include/kadare/kadare.hpp
+++ include/kadare/kadare.hpp
@@ -1,7 +1,7 @@
 #ifndef KANTOKU_DATABASE_READER
 #define KANTOKU_DATABASE_READER
 
-#include <iostream>
+#include <utility>
 #include <string>
 #include <sstream>
 #include <cstdlib>
@@ -10,40 +10,36 @@
 
 namespace kadare{
 
+/* utility functions */
 void deleteSpace(std::string& buf);
 template<typename T> std::vector<T> split(const std::string& str, char delim);
-std::vector<std::string> splitStr(const std::string& str, char delim);
 
+/* DataManager class */
 template<typename Key_type, typename Value_type>
 class DataManager
 {
 public:
 	std::vector<std::vector<Key_type> > key;
 	std::vector<std::vector<Value_type> > value;
-	std::vector<Key_type> key_data;
-	std::vector<Value_type> value_data;
+	std::pair<std::vector<Key_type>, std::vector<Value_type> > data;
 
 	DataManager();
-	DataManager(const std::string& dbdir, const std::string& dbfile);
+	DataManager(const std::string& dbfile);
 	DataManager(const DataManager& obj);
 	~DataManager();
 
-	void load();
+	std::pair<std::vector<Key_type>, std::vector<Value_type> > load(int step=1);
 	void readHeader();
 
 	const std::string getDbfile() const {return dbfile;}
-	const std::string getDbdir() const {return dbdir;}
 	const int getCur() const {return cur;}
 	const int getRecords() const {return records;}
 	const int getKey_dim() const {return key_dim;}
 	const int getValue_dim() const {return value_dim;}
 
 	void setDbfile(const std::string& dbfile);			
-	void setDbfile(const std::string& dbdir, const std::string& dbfile);			
-	void setDbdir(const std::string& dbdir);			
 private:
 	std::string dbfile;
-	std::string dbdir;
 	int cur;
 	int records;
 	int key_dim;
@@ -86,37 +82,20 @@ std::vector<T> split(const std::string& str, char delim)
 	return splited;
 }
 
-std::vector<std::string> splitStr(const std::string& str, char delim)
-{
-	std::vector<std::string> splited;
-	int cur = 0;
-	int found;
-
-	while ((found=str.find_first_of(delim, cur)) != std::string::npos)
-	{
-		splited.push_back(std::string(str, cur, found-cur));
-		cur = found + 1;
-	}
-
-	splited.push_back(std::string(str, cur, str.size()-cur));
-	return splited;
-}
 template<typename Key_type, typename Value_type>
 DataManager<Key_type, Value_type>::DataManager()
 {
-	cur = 0;
+	cur = -1;
 	records = 0;
 	key_dim = 0;
 	value_dim = 0;
 	dbfile = "";
-	dbdir = "./";
 }
 
 template<typename Key_type, typename Value_type>
 DataManager<Key_type, Value_type>::DataManager(const DataManager<Key_type, Value_type>& obj)
 {
 	this->dbfile = obj.getDbfile();
-	this->dbdir = obj.getDbdir();
 	this->cur = obj.getCur();
 	this->records = obj.getRecords();
 	this->key_dim = obj.getKey_dim();
@@ -124,9 +103,8 @@ DataManager<Key_type, Value_type>::DataManager(const DataManager<Key_type, Value
 }
 
 template<typename Key_type, typename Value_type>
-DataManager<Key_type, Value_type>::DataManager(const std::string& dbdir, const std::string& dbfile)
+DataManager<Key_type, Value_type>::DataManager( const std::string& dbfile)
 {
-	setDbdir(dbdir);
 	setDbfile(dbfile);
 }
 
@@ -140,8 +118,7 @@ template<typename Key_type, typename Value_type>
 void DataManager<Key_type, Value_type>::setDbfile(const std::string& dbfile)
 {
 	this->dbfile = dbfile;
-	std::ifstream ifs(dbdir + "/" + dbfile);
-	cur = 0;
+	cur = -1;
 
 	// read header
 	readHeader();
@@ -151,34 +128,25 @@ void DataManager<Key_type, Value_type>::setDbfile(const std::string& dbfile)
 }
 
 template<typename Key_type, typename Value_type>
-void DataManager<Key_type, Value_type>::setDbfile(const std::string& dbdir, const std::string& dbfile)
+std::pair<std::vector<Key_type>, std::vector<Value_type> > DataManager<Key_type, Value_type>::load(int step)
 {
-	setDbdir(dbdir);
-	setDbfile(dbfile);
-}
-
-template<typename Key_type, typename Value_type>
-void DataManager<Key_type, Value_type>::setDbdir(const std::string& dbdir)
-{
-	this->dbdir = dbdir;
-}
-
-template<typename Key_type, typename Value_type>
-void DataManager<Key_type, Value_type>::load()
-{
-	cur++;
-	if (cur >= records)
-	{	// back to begining
-		cur = 0;
+	for (int i=0; i<step; ++i)
+	{
+		cur++;
+		if (cur >= records)
+		{	// back to begining
+			cur = 0;
+		}
 	}
-	key_data = key[cur];
-	value_data = value[cur];
+	data.first = key[cur];
+	data.second = value[cur];
+	return data;
 }
 
 template<typename Key_type, typename Value_type>
 void DataManager<Key_type, Value_type>::readHeader()
 {
-	std::ifstream ifs(dbdir + "/" + dbfile);
+	std::ifstream ifs(dbfile);
 	bool hasHeader = false;
 
 	std::string buf;
@@ -189,7 +157,7 @@ void DataManager<Key_type, Value_type>::readHeader()
 		if (buf[0] == '@')
 		{
 			// header
-			std::vector<std::string> header = splitStr(buf, ',');		
+			std::vector<std::string> header = split<std::string>(buf, ',');		
 			key_dim = std::atoi(header[1].c_str());
 			value_dim = std::atoi(header[2].c_str());
 
@@ -198,7 +166,7 @@ void DataManager<Key_type, Value_type>::readHeader()
 		else if (hasHeader)
 		{
 			// contents
-			std::vector<std::string> content = splitStr(buf, '\t');
+			std::vector<std::string> content = split<std::string>(buf, '\t');
 			key.push_back(split<Key_type>(content[0], ','));
 			value.push_back(split<Value_type>(content[1], ','));
 		}
